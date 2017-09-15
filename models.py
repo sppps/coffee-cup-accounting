c8672a02a89b9cd07a3ed07e42583f539d256517
Sergey Gogin <sppps@sppps.ru>
2017-09-15 14:46:14 +0300

    Models added

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, BigInteger, String, Text, Boolean, Date, DateTime
-from sqlalchemy import ForeignKey, Float, inspect
+from sqlalchemy import ForeignKey, Float, inspect, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime, date, time, timedelta
@@ -40,3 +40,65 @@ class User(Base):
 
     def _get_credentials_string(self, password):
         return '%s:%s' % (self.username, password)
+
+
+class ProductComponentGroup(Base):
+    __tablename__ = 'product_component_groups'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128))
+
+
+class ProductComponent(Base):
+    __tablename__ = 'product_components'
+    id = Column(Integer, primary_key=True)
+    product_component_group_id = Column(Integer, ForeignKey('product_component_groups.id'))
+    product_component_group = relationship("Product")
+    title = Column(String(128))
+    description = Column(Text)
+    units = Column(String(32))
+    current_amount = Column(Float)
+
+
+class ProductComponentSupply(Base):
+    __tablename__ = 'product_component_supplies'
+    id = Column(Integer, primary_key=True)
+    datetime = Column(DateTime, default=datetime.now)
+    product_component_id = Column(Integer, ForeignKey('product_components.id'))
+    product_component = relationship("Product")
+    price = Column(Float)
+    amount = Column(Float)
+
+
+product_components_in_product = Table(
+    'product_components_in_product', Base.metadata,
+    Column('product_id', Integer, ForeignKey('products.id')),
+    Column('product_component_id', Integer, ForeignKey('product_components.id')),
+)
+
+product_component_groups_in_product = Table(
+    'product_component_groups_in_product', Base.metadata,
+    Column('product_id', Integer, ForeignKey('products.id')),
+    Column('product_component_group_id', Integer, ForeignKey('product_component_groups.id')),
+)
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128))
+    components = relationship('ProductComponent', secondary=product_components_in_product)
+    component_grousp = relationship('ProductComponentGroup', secondary=product_component_groups_in_product)
+
+users_in_product_consume = Table(
+    'users_in_product_consume', Base.metadata,
+    Column('product_consumes_id', Integer, ForeignKey('product_consumes.id')),
+    Column('users_id', Integer, ForeignKey('users.id')),
+)
+
+
+class ProductConsume(Base):
+    __tablename__ = 'product_consumes'
+    id = Column(Integer, primary_key=True)
+    datetime = Column(DateTime, default=datetime.now)
+    total_price = Column(Float)
+    consumers = relationship('User', secondary=users_in_product_consume)
