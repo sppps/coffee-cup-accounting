684cb10e4fde8cc10bf01bb87bf1939acbdf55c9
Sergey Gogin <sergey@gogin.pro>
2017-10-16 18:56:54 +0300

    MONGODB_URI option added to config, insted MONGO_DBNAME

--- config/dev_local.py
+++ config/dev_local.py
@@ -2,6 +2,6 @@
 DEBUG = True
 PROJECT_NAME = u'Учет кофе (DEV)'
 SECRET_KEY = '__very_secret_key__change_on_production___'
-MONGO_DBNAME = 'coffee_cup_accounting'
+MONGODB_URI = 'mongodb://127.0.0.1:27017/coffee_cup_accounting'
 ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = '12345'
--- database.py
+++ database.py
@@ -1,13 +1,11 @@
 import pymongo
-import users
+import users.login
 
 
 def init_app(app):
-    mongo_client = pymongo.MongoClient('127.0.0.1')
-    dbnames = mongo_client.database_names()
-    dbname = app.config['MONGO_DBNAME']
-    if app.config['MONGO_DBNAME'] not in dbnames:
-        db = mongo_client[dbname]
+    mongodb_uri = app.config['MONGODB_URI']
+    db = pymongo.MongoClient(mongodb_uri).get_database()
+    admin_user = users.login.User.find(db, username='admin')
+    if admin_user is None:
         users.User.create(db, username='admin', password='123456')
-
-    app.config['db'] = mongo_client[dbname]
+    app.config['db'] = db
